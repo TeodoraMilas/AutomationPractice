bd7c9c5a69e4ddcb0c15376b6988cdf0a9dea15a
teodoraelenamilas <teodora.milas@endava.com>
2020-06-23 14:44:27 +0300

    Just to see if jenkins build is triggered - round 3

--- src/test/java/HerokuTestCases/ProductActions.java
+++ src/test/java/HerokuTestCases/ProductActions.java
@@ -21,6 +21,7 @@ public class ProductActions{
             driver = driverManager.getWebDriver();
             return driver;
         }
+
         WebDriver driverInUse = getDriver();
 
         ProductListPage productList = new ProductListPage(driverInUse);
